f21711793b309d213971778c0cd612678f6b8050
Joe Seongchan <jchan1287@gmail.com>
2020-05-27 03:46:42 +0900

    Class Diagram 추가

--- src/project/controller/qr_inserter/FootNoteQRinserter.java
+++ src/project/controller/qr_inserter/FootNoteQRinserter.java
@@ -1,7 +1,17 @@
 package project.controller.qr_inserter;
 
+import project.model.MyPath;
+import project.model.QRcode;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * 각주로 QR-code 삽입하는 클래스
  */
-public class FootNoteQRinserter {
+public class FootNoteQRinserter extends QRinserter{
+    @Override
+    public void insert(MyPath docPath, ArrayList<ArrayList<QRcode>> qrCodeList) throws IOException {
+
+    }
 }
