7e26d2ba59f47d25321f96101d986c56a0be5c74
Joe Seongchan <jchan1287@gmail.com>
2020-05-27 19:03:46 +0900

    testtest

--- src/project/controller/extractor/Extractor.java
+++ src/project/controller/extractor/Extractor.java
@@ -1,14 +1,16 @@
 package project.controller.extractor;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
 import project.model.MyPath;
 import project.model.Page;
 import project.model.information.Information;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,7 +23,8 @@ abstract public class Extractor {
     protected ArrayList<Page> pageList;         //문서 안 모든 페이지
     protected Pattern pattern;                  //특정한 패턴
     protected ArrayList<ArrayList<Information>> infoList; //문서 안에서 특정한 패턴을 만족하는 모든 문자열 정보
-    protected PDFTextStripper stripper;         //문서를 파싱하는 기계
+    protected Stripper stripper;         //문서를 파싱하는 기계
+    protected ArrayList<List<TextPosition>> listTP = new ArrayList<>();    //TextPostion저장 리스트
 
     /**
      * 생성자
@@ -32,7 +35,7 @@ abstract public class Extractor {
         this.docFile = docFile;
         pageList = new ArrayList<>();
         infoList = new ArrayList<>();
-        stripper = new PDFTextStripper();
+        stripper = new Stripper();
         pattern = Pattern.compile("");
     }
 
@@ -62,11 +65,22 @@ abstract public class Extractor {
 
         System.out.println("-------------[ "+docFile.getPath()+" ] 읽기 시작-------------");
         String buffer = null;
+        List<TextPosition> tempL=null;
+        stripper.setSortByPosition(true);
+        stripper.setStartPage(1);
+        stripper.setEndPage(doc.getNumberOfPages());
+        StringWriter outString=null;
         //한 페이지씩 글자 뽑아내기
         for (int i = 0; i < doc.getNumberOfPages(); i++) {
             //읽을 페이지를 한 쪽으로 한정함.
+            stripper.setSortByPosition(true);
             stripper.setStartPage(i + 1);
             stripper.setEndPage(i + 1);
+            outString = new StringWriter();
+            stripper.writeText(doc, outString);
+            tempL = stripper.getCharactersByArticle().get(0);
+            //System.out.println("temp size:" + tempL.size() + "temp:" + tempL.toString());
+            listTP.add(tempL);
             pageList.add(new Page(i));
             //PDF문서 한 페이지에서 글자 읽기
             try {
@@ -84,6 +98,30 @@ abstract public class Extractor {
             doc.close();
         }catch(IOException e){
             throw new IOException("Extractor::readTexts ERROR: [ " + docFile.getPathString() + " ]을 닫을 수 없습니다.");
+        }
+    }
+
+    private void setPos(){
+        String text;
+        for(int i=0;i<pageList.size();i++){
+            text=pageList.get(i).getText();
+
+            for(int j=0;j<infoList.get(i).size();j++){
+                int infoIndex=text.indexOf(infoList.get(i).get(j).getText())+infoList.get(i).get(j).getText().length()-1;
+                //System.out.println(listTP.get(i).get(infoIndex).toString());
+                infoList.get(i).get(j).setxPos(listTP.get(i).get(infoIndex).getEndX());
+                infoList.get(i).get(j).setyPos(listTP.get(i).get(infoIndex).getEndY());
+                infoList.get(i).get(j).setFontSize(listTP.get(i).get(infoIndex).getFontSize());
+            }
+        }
+
+        for(int i=0;i<pageList.size();i++) {
+            for (int j = 0; j < infoList.get(i).size(); j++) {
+                //System.out.println("text" + infoList.get(i).get(j).getText());
+                //System.out.println("xPos: " + infoList.get(i).get(j).getxPos());
+                //System.out.println("yPos: " + infoList.get(i).get(j).getyPos());
+                //System.out.println("fontSize: " + infoList.get(i).get(j).getFontSize());
+            }
         }
     }
 
@@ -107,5 +145,7 @@ abstract public class Extractor {
                 infoList.get(pageOrder).get(infoOrder).print();
             }
         }
+        setPos();
     }
+
 }
--- src/project/model/information/Information.java
+++ src/project/model/information/Information.java
@@ -9,6 +9,9 @@ public class Information {
     private String text;        //문자열.
     private int textPos;        //문자열의 위치.
     private int order;          //문자열의 순서값.
+    private float xPos;
+    private float yPos;
+    private float fontSize;
 
     /*생성자*/
     public Information(String text, int textPos, int order) {
@@ -50,9 +53,36 @@ public class Information {
         this.order = order;
     }
 
+    public float getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(float xPos) {
+        this.xPos = xPos;
+    }
+
+    public float getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(float yPos) {
+        this.yPos = yPos;
+    }
+
+    public float getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(float fontSize) {
+        this.fontSize = fontSize;
+    }
+
     public void print() {
         System.out.println("\n------[ " + order + " ]번째 link 객체------");
         System.out.println("text: " + text);
         System.out.println("text 위치: " + textPos);
+        System.out.println("xPos: " +   xPos);
+        System.out.println("yPos: " + yPos);
+        System.out.println("fontSize: " + fontSize);
     }
 }
